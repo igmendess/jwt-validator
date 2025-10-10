79eb21e74b42a66c1a6bf1714c9271f280fab35a
igmendess <gr.mendes23@gmail.com>
2025-10-10 11:21:12 -0300

    teste

--- infra/task-definition.tf
+++ infra/task-definition.tf
@@ -9,9 +9,7 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
   container_definitions = jsonencode([
     {
       name = "app-container"
-#       image = "roginhooo/jwt-validator:latest"
-      image = "191384364473.dkr.ecr.us-east-1.amazonaws.com/application-repository:latest"
-#       image = aws_ecr_repository.application_repository.repository_url
+      image = "roginhooo/jwt-validator:latest"
       cpu = 256
       memory = 512
       essential = true
