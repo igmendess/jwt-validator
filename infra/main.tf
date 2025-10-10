9e46dd65e3447650b9533d2c554b42f891b1b472
igmendess <gr.mendes23@gmail.com>
2025-10-10 05:41:28 -0300

    teste

--- infra/main.tf
+++ infra/main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 
 resource "aws_s3_bucket" "igmendess-bucket-terraform-statefile-validator-api" {
   bucket = "bucket-terraform-statefile-validator-api"
-  force_destroy = false  # opcional: permite destruir o bucket mesmo com objetos dentro
+  force_destroy = true  # opcional: permite destruir o bucket mesmo com objetos dentro
 
   tags = {
     Name        = validator-api
